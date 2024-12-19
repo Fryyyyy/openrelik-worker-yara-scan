af8d7c976c81e2aca12571b8dea9c6dd54e6013a
Thomas Chopitea <tomchop@gmail.com>
2024-12-19 14:34:07 +0000

    Cleanup

--- src/providers/yeti.py
+++ src/providers/yeti.py
@@ -6,6 +6,12 @@ from .interface import IntelProvider
 
 
 class YetiIntelProvider(IntelProvider):
+    """Intel provider for the Yeti platform.
+
+    Attrs:
+        yeti_client: Yeti API client.
+    """
+
     def __init__(self):
         endpoint = os.environ.get("YETI_ENDPOINT")
         api_key = os.environ.get("YETI_API_KEY")
@@ -16,9 +22,16 @@ class YetiIntelProvider(IntelProvider):
         self.yeti_client = YetiApi(endpoint)
         self.yeti_client.auth_api_key(api_key)
 
-    def get_yara_rules(self) -> str:
-        indicators = self.yeti_client.search_indicators(indicator_type="yara")
+    def get_yara_rules(self, name_filter: str) -> str:
+        """Get all YARA rules from the Yeti platform.
+
+        Args:
+            name_filter: Filter for Yara rule names.
+        """
+        indicators = self.yeti_client.search_indicators(
+            name=name_filter, indicator_type="yara"
+        )
         all_patterns = ""
-        for i in indicators:
-            all_patterns += i["pattern"] + "\n\n"
+        for indicator in indicators:
+            all_patterns += indicator["pattern"] + "\n\n"
         return all_patterns
